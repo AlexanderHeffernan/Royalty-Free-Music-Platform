6844da08a9a324bbdb85a6ac42e431e25fae9bd9
AlexanderHeffernan <alexander.heffernan@stream.school.nz>
2021-06-08 10:10:38 +1200

    Website slogan edited

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <div class="header-container">
                 <h1>
                 The World's Largest 
-                Royalty Free Music Library
+                Royalty Free Music Library – That is free!
                 </h1>
             </div>
             <div class="search">
