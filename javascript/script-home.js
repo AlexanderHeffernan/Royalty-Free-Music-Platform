a15b2212293e9b2c27db12869466a04c0f510f33
AlexanderHeffernan <alexander.heffernan@stream.school.nz>
2021-06-08 18:30:35 +1200

    clear

--- javascript/script-home.js
+++
@@ -1,9 +0,0 @@
-// WAVE EFFECTS //
-Waves.attach('button', ['waves-effect', 'waves-light']);
-Waves.attach('.ripple', ['waves-effect', 'waves-light']);
-Waves.attach('#myUL Li', ['waves-effect', 'waves-light']);
-Waves.attach('.active', ['waves-effect', 'waves-dark']);
-Waves.attach('#sort', ['waves-effect', 'waves-dark']);
-Waves.attach('#sort option', ['waves-effect', 'waves-dark']);
-
-Waves.init();
