c91d089a832c97fc82e741e96b17ae14b58a53b0
AlexanderHeffernan <alexander.heffernan@stream.school.nz>
2021-06-08 19:33:41 +1200

    Edited home page text

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <div class="header-container">
                 <h1>
                 The World's Largest 
-                Royalty Free Music Library – That is free!!!
+                Royalty Free Music Library – That is free!!! I agree!
                 </h1>
             </div>
             <div class="search">
