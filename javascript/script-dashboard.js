011090b94f690dcc783c3977f5f7772ddcb26189
AlexanderHeffernan <alexander.heffernan@stream.school.nz>
2021-06-12 00:46:31 +1200

    Fixed formatting

--- javascript/script-dashboard.js
+++ javascript/script-dashboard.js
@@ -1,54 +1,51 @@
 // WAVE EFFECTS //
-Waves.attach('button', ['waves-effect', 'waves-light']);
-Waves.attach('.bottom-btn', ['waves-effect', 'waves-light']);
-Waves.attach('.ripple', ['waves-effect', 'waves-light']);
-Waves.attach('.playmusic', ['waves-effect', 'waves-light']);
-Waves.attach('.active', ['waves-effect', 'waves-dark']);
-Waves.attach('#sort', ['waves-effect', 'waves-dark']);
-Waves.attach('#sort option', ['waves-effect', 'waves-dark']);
+Waves.attach("button", ["waves-effect", "waves-light"]);
+Waves.attach(".bottom-btn", ["waves-effect", "waves-light"]);
+Waves.attach(".ripple", ["waves-effect", "waves-light"]);
+Waves.attach(".playmusic", ["waves-effect", "waves-light"]);
+Waves.attach(".active", ["waves-effect", "waves-dark"]);
+Waves.attach("#sort", ["waves-effect", "waves-dark"]);
+Waves.attach("#sort option", ["waves-effect", "waves-dark"]);
 
 Waves.init();
-
-
 
 const filterOne = document.querySelector(".filter-button-one");
 const filterTwo = document.querySelector(".filter-button-two");
 
-var clickOne = false
+var clickOne = false;
 function filterClickOne() {
-    if (clickOne == false) {
-        filterOne.classList.add('material-icons-clicked')
-        clickOne = true;
-    } else {
-        filterOne.classList.remove('material-icons-clicked')
-        clickOne = false;
-    }
+  if (clickOne == false) {
+    filterOne.classList.add("material-icons-clicked");
+    clickOne = true;
+  } else {
+    filterOne.classList.remove("material-icons-clicked");
+    clickOne = false;
+  }
 }
-var clickTwo = false
+var clickTwo = false;
 function filterClickTwo() {
-    if (clickTwo == false) {
-        filterTwo.classList.add('material-icons-clicked')
-        clickTwo = true;
-    } else {
-        filterTwo.classList.remove('material-icons-clicked')
-        clickTwo = false;
-    }
+  if (clickTwo == false) {
+    filterTwo.classList.add("material-icons-clicked");
+    clickTwo = true;
+  } else {
+    filterTwo.classList.remove("material-icons-clicked");
+    clickTwo = false;
+  }
 }
-
 
 const profileSettings = document.querySelector(".profile-settings");
-var clickThree = false
+var clickThree = false;
 function profileSettingsFunction() {
-    if (clickThree == false) {
-        profileSettings.classList.add('clicked')
-        clickThree = true;
-    }
+  if (clickThree == false) {
+    profileSettings.classList.add("clicked");
+    clickThree = true;
+  }
 }
 function profileSettingsFunctionHide() {
-    if (clickThree == true) {
-        profileSettings.classList.remove('clicked')
-        clickThree = false;
-    }
+  if (clickThree == true) {
+    profileSettings.classList.remove("clicked");
+    clickThree = false;
+  }
 }
 
 var recentSongName = myTitles[myTitles.length - 1];
@@ -60,18 +57,20 @@ var recentSongDownloads = myDownloads[myDownloads.length - 1];
 document.getElementById("recentSongCover").src = recentSongCover;
 document.getElementById("recentSongName").innerHTML = recentSongName;
 document.getElementById("recentSongArtist").innerHTML = recentSongArtist;
-document.getElementById("recentSongListens").innerHTML = recentSongListen + " Listens";
-document.getElementById("recentSongDownloads").innerHTML = recentSongDownloads + " Downloads";
+document.getElementById("recentSongListens").innerHTML =
+  recentSongListen + " Listens";
+document.getElementById("recentSongDownloads").innerHTML =
+  recentSongDownloads + " Downloads";
 
 var totalListens = 0;
-for(var i = 0; i < myListens.length; i++) {
-    totalListens += myListens[i];
+for (var i = 0; i < myListens.length; i++) {
+  totalListens += myListens[i];
 }
 document.getElementById("totalListens").innerHTML = totalListens;
 
 var totalDownloads = 0;
-for(var i = 0; i < myDownloads.length; i++) {
-    totalDownloads += myDownloads[i];
+for (var i = 0; i < myDownloads.length; i++) {
+  totalDownloads += myDownloads[i];
 }
 document.getElementById("totalDownloads").innerHTML = totalDownloads;
 
