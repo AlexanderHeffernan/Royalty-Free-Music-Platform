72bdfba482bcdb4a9ff05524567f808081adfd24
lokeshdhade <lokesh.dhade@stream.school.nz>
2021-08-13 09:43:00 +1200

    tobey's about page

--- about.php
+++ about.php
@@ -13,12 +13,58 @@
 
     <!-- content -->
 <div class="main-container" id="main-container"> 
-    <div class="hero">
-        <div class="hero-content">
-        <p>rfm</p>
+    <div class="head">
+        <div class="head-container">
+            <div class="head-content"> 
+                <p class="head-text">Who are we?</p>
+            </div>            
         </div>
-
     </div>
+    <div class="features">
+        <p class="featureheadtext">Who are we?</p>
+
+        <div class="features-container">
+            <div class="features-content">
+                <img class="imagepannel border1" src="https://i.imgur.com/AspwLAO.png" alt="">
+                <div class="textpannel">
+                    <p>is it dog</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="features2">
+        <div class="features-container">
+            <div class="features-content">
+                <div class="textpannel">
+                    <p>is it dog</p>
+                </div>
+                <img class="imagepannel border2" src="https://i.imgur.com/AspwLAO.png" alt="">
+
+            </div>
+        </div>
+    </div>
+
+    <section class="herocontainer">
+        <div class="hero">
+        <div class="container">
+            <div class="header-container">
+                <h1>
+                What are you waiting for?
+                </h1>
+            </div>
+            <div class="search">
+                <form action="explore.php" method="POST">
+                    <input type="text" name="search" placeholder="What mood are you looking for?...">
+                    <button type="submit" name="submit-search" class="material-icons search-icon">search</button> 
+                </form>
+            </div>
+        
+        </div>
+        </div>
+    </section>
+
+
 </div>
 
 
